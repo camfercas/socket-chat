aff2c765a5f86f31da00326493e1a3ceb8026db2
Camilo <camfercas@gmail.com>
2020-08-25 17:07:24 -0300

    Primer commit

---
+++ public/js/socket-chat.js
@@ -0,0 +1,53 @@
+var socket = io();
+
+var params = new URLSearchParams(window.location.search);
+
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesarios');
+}
+
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
+
+socket.on('connect', function() {
+    socket.emit('entrarChat', usuario, function(resp) {
+        console.log('Usuarios conectados', resp);
+    });
+});
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
+socket.emit('enviarcMensaje', {
+    usuario: 'Fernando',
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log('respuesta server: ', resp);
+});
+
+// Escuchar información
+socket.on('crearMensaje', function(mensaje) {
+
+    console.log('Servidor:', mensaje);
+
+});
+
+socket.on('listaPersonas', function(mensaje) {
+
+    console.log('Servidor:', mensaje);
+
+});
+
+// Mensajes pribvados
+socket.on('mensajePrivado', function(mensaje) {
+    console.log('Mensaje Privado', mensaje);
+});
